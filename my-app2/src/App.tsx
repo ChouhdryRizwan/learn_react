ebcf7d40a7bc594d567f21867686eb1c467d86af
ChouhdryRizwan <muhammadrizwantahir23@gmail.com>
2024-01-03 22:35:49 +0500

    use-state

--- my-app2/src/App.tsx
+++ my-app2/src/App.tsx
@@ -1,10 +1,14 @@
-import React from "react";
+import { useState } from "react";
 
 function App() {
   let a = 2 + 2;
+  const myarr = useState<number>(10);
+  let myValue = myarr[0];
+  let setValue = myarr[1];
   return (
     <div>
-      <p>number: {a}</p>
+      <p>number: {myValue}</p>
+      {/* <p>number: {a}</p> */}
       <button onClick={() => a++}>click me</button>
       <button onClick={() => console.log(a)}>log value</button>
     </div>
